4ed65c7ee680f788fa2dc6ddb4d5544d594dc4cb
Sandro Lovato <56365829+SandroLovato@users.noreply.github.com>
2022-03-10 13:32:31 -0300

    Update correçãoVB.sh

--- correçãoVB.sh
+++ correçãoVB.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 clear
 echo "Corrigr erro VirtualBox Guest Additions installation problem"
-sudo apt-get update
-sudo apt-get install build-essential gcc make perl dkms
-sudo /sbin/vboxconfig
+sudo mount /dev/cdrom /mnt
+##Listando o conteúdo do ponto de montagem:##
+ls -l /mnt
+##Instale os pacotes necessários##
+sudo apt update -y && sudo apt upgrade
+sudo apt install dkms linux-headers-$(uname -r) build-essential
+##Instalar adições de convidados do VirtualBox no DEbian##
+sudo sh /mnt/VBoxLinuxAdditions.run
+##Após a instalação, reinicie sua máquina virtual para recarregar os módulos do kernel.##
+systemctl reboot -i
 echo ""
-sudo reboot
